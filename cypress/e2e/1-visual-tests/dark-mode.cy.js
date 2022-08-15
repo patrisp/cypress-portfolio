11cd58ffc40894ec4cdf3fe62a1f31f7c67aafc9
Patrycja Pawlowska <patrycja.pawlowska@spaceos.io>
2022-08-15 21:59:42 +0200

    dark mode tests

---
+++ cypress/e2e/1-visual-tests/dark-mode.cy.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+
+describe('Verify that dark mode is working correctly', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        cy.closePolicy();
+    });
+
+    it('Light mode is set as the default mode' ,() => {
+        cy.get('#logo-justjoin-it').should('have.css', 'fill', 'rgb(55, 71, 79)'); 
+        cy.get('header > div').should('have.css', 'background-color', 'rgb(255, 255, 255)');
+        cy.screenshot({capture: 'fullPage'});
+    });
+
+    it('Background and font colours are correct for the dark mode' ,() => {
+        cy.get('[type="checkbox"]').eq(0).check();
+        cy.get('#logo-justjoin-it').should('have.css', 'fill', 'rgba(255, 255, 255, 0.8)'); 
+        cy.get('header > div').should('have.css', 'background-color', 'rgb(44, 44, 44)');
+        cy.screenshot({capture: 'fullPage'});
+    });
+
+    it('Dark mode is enabled in all tabs' ,() => {
+        cy.get('[type="checkbox"]').eq(0).check();
+
+        cy.visit('/brands');
+        cy.get('#logo-justjoin-it').should('have.css', 'fill', 'rgba(255, 255, 255, 0.8)'); 
+        cy.get('header > div').should('have.css', 'background-color', 'rgb(44, 44, 44)');
+
+        cy.visit('https://geek.justjoin.it/');
+        cy.get('#masthead').should('have.css', 'color', 'rgb(68, 68, 68)');
+
+        cy.visit('/matchmaking');
+        cy.get('#logo-justjoin-it').should('have.css', 'fill', 'rgba(255, 255, 255, 0.8)'); 
+        cy.get('header > div').should('have.css', 'background-color', 'rgb(44, 44, 44)');
+
+        cy.visit('/add-offer'); //test fails - bug found
+        cy.get('#logo-justjoin-it').should('have.css', 'fill', 'rgba(255, 255, 255, 0.8)'); 
+        cy.get('header > div').should('have.css', 'background-color', 'rgb(44, 44, 44)');
+    });
+
+    it('Pop-ups are displayed in dark mode', () => {
+        cy.get('[type="checkbox"]').eq(0).check();
+
+        cy.get('button').contains('Sign in').click({timeout: 3000}); //'Sign in' pop-up
+        cy.get('div[class^="MuiPaper-root"], [class^="MuiPopover-paper"]').eq(1).as('popover');
+        cy.get('@popover').should('be.visible').and('have.css', 'background-color', 'rgb(44, 44, 44)');
+        cy.get('body').click(0,0);
+
+        cy.get('.css-plb9h9 > button').click({timeout: 3000}); //'Locations' filter pop-up
+        cy.get('@popover').should('be.visible').and('have.css', 'background-color', 'rgb(44, 44, 44)');
+        cy.get('body').click(0,0);
+
+        cy.get('button').contains('PLN').click({timeout: 3000}); //'Currency' pop-up
+        cy.get('@popover').should('be.visible').and('have.css', 'background-color', 'rgb(44, 44, 44)');
+        cy.get('body').click(0,0);
+        
+        cy.get('[d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"]').parentsUntil('button').eq(1).click({timeout: 3000}); // sidebar pop-up
+        cy.get('@popover').should('be.visible').and('have.css', 'background-color', 'rgb(44, 44, 44)');
+    });
+
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,30 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//close cookies information pop-up
+Cypress.Commands.add('closePolicy', () =>{
+    cy.get('.jss231 > .MuiButtonBase-root-78 > .MuiButton-label-201').click();
+});
